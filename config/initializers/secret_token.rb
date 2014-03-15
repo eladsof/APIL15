86303bada93dbab3df228330140bd7e1e20732a2
Elad Sofer <elad.sofer@gmail.com>
2014-03-15 15:19:02 +0200

    Empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apilsite::Application.config.secret_token = 'd4e29b814c7ca09cf4004b2803825fb9c86b4d82061d667ac9b7085edc57b42b1d38fc122870cb318144942f83ba0cc386c2de2fae02e528a5f5d5fe68bc6b38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Apilsite::Application.config.session_store :cookie_store, key: '_apilsite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Apilsite::Application.config.session_store :active_record_store
