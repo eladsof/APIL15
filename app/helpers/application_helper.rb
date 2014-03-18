0c4a62d9e67c61c42d800fdb13512b429ceed73a
Elad Sofer <elad.sofer@gmail.com>
2014-03-18 21:05:09 +0200

    no model yet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Agile Practitioners 2015 conference"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
