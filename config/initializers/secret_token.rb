39028f2bd3519b2b113d69566f20ffca593eb86a
Elad Sofer <elad.sofer@gmail.com>
2014-03-20 11:33:57 +0200

    Trying again...

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Apilsite::Application.config.secret_token = 'd4e29b814c7ca09cf4004b2803825fb9c86b4d82061d667ac9b7085edc57b42b1d38fc122870cb318144942f83ba0cc386c2de2fae02e528a5f5d5fe68bc6b38'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apilsite::Application.config.secret_key_base = '4acaa154278300ca7cf3f8e05f5e6535c24cf80b3f460409a9e4ef6aaaca0bb99d672bf4cfb9429a7b4365fc73f37a02c249b3741d7d453c53af0d8bf0c578b5'
